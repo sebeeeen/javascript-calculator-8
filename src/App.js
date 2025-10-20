a3b12f06d0c7d321497d012c2e43125ceb2578e7
sebeeeen <a856412@gmail.com>
2025-10-20 13:04:11 +0900

    feat: 문자열 덧셈 계산기 구현
    - 빈 문자열 입력 시 0 반환 기능 구현
    - 기본 구분자(쉼표, 콜론)로 문자열 분리 기능 구현
    - 커스텀 구분자 파싱 기능 구현
    - 음수 입력 시 에러 발생 기능 구현
    - 숫자가 아닌 값 입력 시 에러 처리 구현
    - Airbnb 스타일 가이드 준수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,65 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    
+    try {
+      const result = this.calculate(input);
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  calculate(input) {
+    if (input === '') {
+      return 0;
+    }
+
+    const { delimiter, numbers } = this.parseInput(input);
+    const numberArray = this.parseNumbers(numbers, delimiter);
+    
+    return numberArray.reduce((sum, num) => sum + num, 0);
+  }
+
+  parseInput(input) {
+    let delimiter = /[,:]/;
+    let numbers = input;
+
+    if (input.startsWith('//')) {
+      const delimiterEndIndex = input.indexOf('\\n');
+      const customDelimiter = input.substring(2, delimiterEndIndex);
+      delimiter = new RegExp(`[,:${this.escapeRegExp(customDelimiter)}]`);
+      numbers = input.substring(delimiterEndIndex + 2);
+    }
+
+    return { delimiter, numbers };
+  }
+
+  parseNumbers(numbers, delimiter) {
+    return numbers.split(delimiter).map((num) => {
+      const parsed = Number(num);
+      
+      this.validateNumber(parsed);
+      
+      return parsed;
+    });
+  }
+
+  validateNumber(num) {
+    if (Number.isNaN(num)) {
+      throw new Error('[ERROR] 숫자가 아닌 값이 포함되어 있습니다.');
+    }
+    
+    if (num < 0) {
+      throw new Error('[ERROR] 음수는 입력할 수 없습니다.');
+    }
+  }
+
+  escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
 }
 
 export default App;
